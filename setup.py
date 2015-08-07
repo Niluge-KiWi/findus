bf11c60cc175c4b22d8731206cdf1bcfca2e38eb
Thomas Riccardi <riccardi.thomas@gmail.com>
2015-08-07 20:57:38 +0200

    Add setup.py for dependencies on docopt & simplejson

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='findus',
+      version='0.1',
+      description='Reduce debt graphs',
+      author='',
+      author_email='',
+      url='https://github.com/gapato/findus',
+      install_requires=['docopt', 'simplejson'],
+     )
